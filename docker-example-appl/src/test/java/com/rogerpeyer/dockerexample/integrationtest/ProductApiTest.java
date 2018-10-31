8407aee33d012fda40c8ffb9f1af8ffa5c762255
Roger Peyer <peyer.roger@gmail.com>
2018-10-31 23:20:32 +0100

    upgrade gradle use log4j2 add Kafka

--- docker-example-appl/src/test/java/com/rogerpeyer/dockerexample/integrationtest/ProductApiTest.java
+++ docker-example-appl/src/test/java/com/rogerpeyer/dockerexample/integrationtest/ProductApiTest.java
@@ -13,10 +13,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka
 public class ProductApiTest {
 
   @Autowired
